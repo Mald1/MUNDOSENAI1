7737b992ecb1eff6054364d848776c0850445d98
Joao <jpmaldo1@gmail.com>
2024-09-04 10:07:21 -0300

    FrontTelaDeLogs

--- MundoSENAI/src/App.jsx
+++ MundoSENAI/src/App.jsx
@@ -6,6 +6,7 @@ import Alunos from "./pages/Alunos/Alunos.jsx";
 import MundoSENAI from "./pages/MundoSenai/MundoSENAI.jsx";
 import Refeicoes from "./pages/Refeicoes/Refeicoes.jsx";
 import Tutorial from "./pages/Tutorial/Tutorial.jsx";
+import LogAccessPage from "./pages/LogAccessPage/LogAccessPage.jsx";
 
 function App() {
   const [eventos, setEventos] = useState([
@@ -23,6 +24,7 @@ function App() {
         <Route path="/mundo-senai" element={<MundoSENAI />} />
         <Route path="/refeicoes" element={<Refeicoes eventos={eventos} />} />
         <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/logs" element={<LogAccessPage />} />
       </Routes>
     </Router>
   );
--- MundoSENAI/src/pages/Inicio/Inicio.jsx
+++ MundoSENAI/src/pages/Inicio/Inicio.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './Inicio.css';
+import { Link } from "react-router-dom";
 
 function Inicio() {
   return (
     <div className="inicio-container">
       <h1>Organização de Eventos - SENAI</h1>
       <p>Bem-vindo ao sistema de organização de eventos do SENAI. Aqui você pode gerenciar alunos, eventos e refeições de forma eficiente.</p>
-      <button className="logs-button">Acessar Logs</button>
+      <Link to="/logs"><button className="logs-button">Acessar Logs</button></Link>
     </div>
   );
 }
